b3bd55df8c44329f63d26c3d889b09e08869d57b
ted-han <taekyung28h@gmail.com>
2021-03-11 21:26:20 -0500

    table component

---
+++ src/components/table.js
@@ -0,0 +1,71 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Layout = ({ data, category }) => {
+  const order = {
+    food: ["item", "health", "stamina", "duration", "weight", "craft"],
+  }
+  const itemOrder = order[category]
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            {itemOrder.map((v, i) => (
+              <td key={i}>{v}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.allItems.edges.map(({ node }) => {
+            return (
+              <tr key={node.id}>
+                {itemOrder.map((v, i) => {
+                  if (v === "item") {
+                    let imgName = node.name.replace(/ /gi, "_")
+                    return (
+                      <td key={i}>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <img
+                            src={`${imgName}.png`}
+                            alt={imgName}
+                            height="50px"
+                          />
+                          <Link
+                            to={`/item/${
+                              node.category
+                            }/${node.name.toLowerCase().replace(/ /gi, "-")}`}
+                          >
+                            {node.name}
+                          </Link>
+                        </div>
+                      </td>
+                    )
+                  } else if (v === "craft" && node.data.craft) {
+                    return (
+                      <td key={i}>
+                        {node.data.craft.map((v, i) => (
+                          <span key={i}>
+                            <img
+                              src={`${v.name}.png`}
+                              alt={v.name}
+                              height="50px"
+                            />
+                            x{v.count}
+                          </span>
+                        ))}
+                      </td>
+                    )
+                  }
+                  return <td key={i}>{node.data[v]}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Layout
