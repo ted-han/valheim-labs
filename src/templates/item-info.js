82b1a5234dcc12dc223c5a021302657c89b7b11d
ted-han <taekyung28h@gmail.com>
2021-03-13 23:41:34 -0500

    item info page draft

--- src/templates/item-info.js
+++ src/templates/item-info.js
@@ -6,11 +6,27 @@ export default function BlogPost({ data }) {
   const item = data.items
   return (
     <Layout>
-      <div>
-        <h1>{item.id}</h1>
-        <h1>{item.name}</h1>
-        <h1>{item.category}</h1>
+      {item.category} > {item.name}
+      <br /> image, name, desc, data, collected(biomes), ingredient, crafting
+      <div style={{ display: "flex", border: "1px solid black" }}>
+        <div>
+          <h1>{item.name}</h1>
+          <p>{item.desc}</p>
+          <p>{item.collected}</p>
+          {Object.keys(item.data).map(v => {
+            if (v === "craft") return ""
+            return (
+              <p>
+                {v}: {item.data[v]}
+              </p>
+            )
+          })}
+        </div>
+        <div> image</div>
       </div>
+      <p>{item.ingredient}</p>
+      <p>{item.craft}</p>
+      <p>{item.with}</p>
     </Layout>
   )
 }
@@ -18,8 +34,21 @@ export default function BlogPost({ data }) {
 export const query = graphql`
   query($id: String!) {
     items(id: { eq: $id }) {
-      id
+      with
       name
+      desc
+      data {
+        craft {
+          count
+          name
+        }
+        duration
+        healing
+        health
+        stamina
+        weight
+      }
+      craft
       category
     }
   }
